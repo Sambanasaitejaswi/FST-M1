52b092e905f62b365045cb79e107282416599784
Sambanasaitejaswi <Saitejaswi.Sambana@ibm.com>
2024-09-05 18:54:02 +0530

    Add files via upload

---
+++ Java/Activities/Car.java
@@ -0,0 +1,35 @@
+package Activities;
+
+
+public class Car {
+    //Class Member Variables
+    String color;
+    int make;
+    String transmission;
+    int tyres;
+    int doors;
+
+    //Constructor
+    Car() {
+        tyres = 4;
+        doors = 4;
+    }
+
+    //Class Methods
+    public void displayCharacteristics() {
+        System.out.println("Color of the car: " + color);
+        System.out.println("Make of the car: " + make);
+        System.out.println("Transmission of the car: " + transmission);
+        System.out.println("Number of doors on the car: " + doors);
+        System.out.println("Number of tyres on the car: " + tyres);
+    }
+
+    public void accelerate() {
+        System.out.println("car is moving forward.");
+    }
+
+    public void brake() {
+        System.out.println("car has stopped.");
+    }
+
+}
